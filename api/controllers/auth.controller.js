0faf006a053b66e4075e61bb08549f4b188df0c8
Itsuki06 <trinhtung0604@gmail.com>
2025-09-30 23:53:25 +0700

    4 - Add lib/prisma, ttiny change register api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
-    
+    try {
     // DB operater
     console.log(req.body);
 
@@ -12,6 +13,17 @@ export const register = async (req, res) => {
     console.log(hashedPassword);
     
     // Store user in the database
+    const newUser = await prisma.user.create({ 
+        data: { 
+            username, 
+            email, 
+            password: hashedPassword 
+        },
+    });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to sign up!' });
+    }
 }
 
 export const login = (req, res) => {
