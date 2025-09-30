0a6927b81f5ca8760de601d447949e71d0383073
Itsuki06 <trinhtung0604@gmail.com>
2025-09-30 23:55:49 +0700

    5 - Add login api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,31 +3,54 @@ import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
-    try {
-    // DB operater
-    console.log(req.body);
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    console.log(hashedPassword);
     
-    // Store user in the database
-    const newUser = await prisma.user.create({ 
-        data: { 
-            username, 
-            email, 
-            password: hashedPassword 
-        },
-    });
+    try {
+        // DB operater
+        console.log(req.body);
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        console.log(hashedPassword);
+        
+        // Store user in the database
+        const newUser = await prisma.user.create({ 
+            data: { 
+                username, 
+                email, 
+                password: hashedPassword 
+            },
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Failed to sign up!' });
     }
 }
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
     //db operater
+    const { username, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { username: username },
+        });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid Credentials!' });
+        }
+
+        const isPasswordValid = await bcrupt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid Credentials!' });
+        }
+
+    
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to log in!' }); 
+    }
 }
 
 export const logout = (req, res) => {
